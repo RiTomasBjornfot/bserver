7ee867fe172117bc3f64808cc977827a3e39edd8
Tomas Bjornfot <tomas@localhost.localdomain>
2026-02-20 14:47:23 +0100

    removed chatgpt shit

--- projrouter/run.sh
+++
@@ -1,5 +0,0 @@
-# För att skriva /etc/nginx och /etc/systemd behöver du sudo
-sudo python3 main.py --registry /home/<user>/servers/registry.toml activate projekt1 9001
-sudo python3 main.py --registry /home/<user>/servers/registry.toml check
-sudo python3 main.py --registry /home/<user>/servers/registry.toml deactivate projekt1
-
